7ebfe755938e2e28bbf2b03af2c63ca4e64450b2
Elliott0121 <44977939+Elliott0121@users.noreply.github.com>
2020-06-05 13:34:38 +0200

    Added animate.css to dependencies

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from 'react-dom';
 import App from './App.js';
 import './styles/stylesheet.css';
+import 'animate.css';
 
 ReactDOM.render(<App />, document.getElementById("root"));
